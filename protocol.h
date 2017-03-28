15f766971da37ae5437d1f23104824d6f00851ec
dreamwjw <609039113@qq.com>
2017-03-28 20:10:45 +0800

    modify protocol.h and server.cpp

--- protocol.h
+++ protocol.h
@@ -1,40 +1,43 @@
 #ifndef PROTOCOL_H
 #define PROTOCOL_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 #pragma pack(push)
 #pragma pack (1)
 
-typedef unsigned char          uint8_t;
-typedef unsigned short         uint16_t;
-typedef unsigned int           uint32_t;
-typedef unsigned long long     uint64_t;
-typedef char                   int8_t;
-typedef short                  int16_t;
-typedef int                    int32_t;
-typedef long long              int64_t;
+typedef unsigned char          WU_uint8_t;
+typedef unsigned short         WU_uint16_t;
+typedef unsigned int           WU_uint32_t;
+typedef unsigned long long     WU_uint64_t;
+typedef char                   WU_int8_t;
+typedef short                  WU_int16_t;
+typedef int                    WU_int32_t;
+typedef long long              WU_int64_t;
 
 #define LOGIN_REQ 0x2001                   // Client -> Server  --- struct LoginReq
 #define LOGIN_RSP (LOGIN_REQ + 1)          // Server -> Client  --- struct LoginRsp
 
 typedef struct tagHeader
 {
-	uint8_t szFlag[4];                     //default "WULI"
-	uint16_t usCode;                       //Request code
-	uint32_t uiTotalLength;                //Total packet length(header+data)
-	uint64_t ullSrcId;                     //source ID
-	uint64_t ullDstId;                     //destination ID
+	WU_uint8_t szFlag[4];                     //default "WULI"
+	WU_uint16_t usCode;                       //Request code
+	WU_uint32_t uiTotalLength;                //Total packet length(header+data)
+	WU_uint64_t ullSrcId;                     //source ID
+	WU_uint64_t ullDstId;                     //destination ID
 }Header;
 
 typedef struct tagLoginReq
 {
-	uint8_t szUserName[32];               //user name 
-	uint8_t szPassWord[32];               //password
+	WU_uint8_t szUserName[32];               //user name 
+	WU_uint8_t szPassWord[32];               //password
 }LoginReq;
 
 typedef struct tagLoginRsp
 {
-	uint8_t ucResult;                     //0-success£¬1-user name error£¬2-password error
-	uint8_t szReason[128];
+	WU_uint8_t ucResult;                     //0-success£¬1-user name error£¬2-password error
+	WU_uint8_t szReason[128];
 }LoginRsp;
 
 #pragma pack(pop)
