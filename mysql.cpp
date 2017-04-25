1d580dd22f232d5d42d336ab9085cb2f59807ce2
dreamwjw <609039113@qq.com>
2017-04-25 20:10:27 +0800

    add CMysql class

---
+++ mysql.cpp
@@ -0,0 +1,92 @@
+#include <stdio.h>
+
+#include "mysql.h"
+
+CMysql* CMysql::m_pCMysql = NULL;
+
+CMysql::CMysql()
+{
+	char server [] = "localhost", user [] = "root", password [] = "Wjw7132131",
+	     database [] = "server";
+
+	m_pMysql = mysql_init(NULL);
+	if (!mysql_real_connect(m_pMysql, server, user, password, database, 0, NULL, 0))
+	{
+		printf("%s\n", mysql_error(m_pMysql));
+		exit(1);
+	}
+}
+
+CMysql::~CMysql()
+{
+	mysql_close(m_pMysql);
+	
+	if (m_pCMysql != NULL)
+	{
+		delete m_pCMysql;
+		m_pCMysql = NULL;
+	}
+}
+
+CMysql* CMysql::GetInstance()
+{
+	if (m_pCMysql == NULL) m_pCMysql = new CMysql();
+	
+	return m_pCMysql;
+}
+
+int CMysql::mysql_GetUserID(const char* szUserName, const char* szPassWord)
+{
+	return 0;
+}
+
+//int mysql_select(MYSQL *conn, const char *szSelect, char* szResult) 
+//{
+//	MYSQL_RES *res;
+//	MYSQL_FIELD *fields;
+//	MYSQL_ROW row;
+//
+//	if (mysql_query(conn, szSelect))
+//	{
+//		printf("%s\n", mysql_error(conn));
+//		return -1;
+//	}
+//
+//	res = mysql_use_result(conn);
+//	int nFields = mysql_num_fields(res);
+//
+//	char szTempBuffer[32] = {0};
+//	fields = mysql_fetch_fields(res);
+//	for (int i = 0; i < nFields; i++)
+//	{
+//		sprintf(szTempBuffer, "%s ", fields[i].name);
+//		strcat(szResult, szTempBuffer);
+//	}
+//
+//	while ((row = mysql_fetch_row(res)) != NULL)
+//	{
+//		for (int i = 0; i < nFields; i++)
+//		{
+//			sprintf(szTempBuffer, "%s ", row[i]);
+//			strcat(szResult, szTempBuffer);
+//		}
+//	}
+//
+//	mysql_free_result(res);
+//
+//	return 0;
+//}
+//
+//int mysql_noselect(MYSQL *conn, const char* szNoSelect, char* szResult) 
+//{
+//	if (mysql_query(conn, szNoSelect))
+//	{
+//		printf("%s\n", mysql_error(conn));
+//		return -1;
+//	}
+//
+//	int nAffectedRow =  mysql_affected_rows(conn);
+//	sprintf(szResult, "%d rows affected\n", nAffectedRow);
+//
+//	return 0;
+//}
--- server.cpp
+++ server.cpp
@@ -2,83 +2,84 @@
 
 void *workthread(void *socket);
 
-int main(int argc, char **argv) {
-  struct sockaddr_in server_addr;
-  int server_socket;
-  int opt = 1;
+int main(int argc, char **argv)
+{
+	struct sockaddr_in server_addr;
+	int server_socket;
+	int opt = 1;
 
-  bzero(&server_addr, sizeof(server_addr));
+	bzero(&server_addr, sizeof(server_addr));
 
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = htons(INADDR_ANY);
-  server_addr.sin_port = htons(HELLO_WORLD_SERVER_PORT);
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = htons(INADDR_ANY);
+	server_addr.sin_port = htons(HELLO_WORLD_SERVER_PORT);
 
-  /* create a socket */
-  server_socket = socket(PF_INET, SOCK_STREAM, 0);
-  if(server_socket < 0)
-  {
-    printf("Create Socket Failed!\n");
-    exit(1);
-  }
-  printf("Create Socket Success!\n");
+	  /* create a socket */
+	server_socket = socket(PF_INET, SOCK_STREAM, 0);
+	if (server_socket < 0)
+	{
+		printf("Create Socket Failed!\n");
+		exit(1);
+	}
+	printf("Create Socket Success!\n");
 
-  /* bind socket to a specified address */
-  setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-  if(bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)))
-  {
-    printf("Server Bind Port : %d Failed!\n", HELLO_WORLD_SERVER_PORT);
-    exit(1);
-  }
-  printf("Server Bind Port : %d Success!\n", HELLO_WORLD_SERVER_PORT);
+	  /* bind socket to a specified address */
+	setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+	if (bind(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)))
+	{
+		printf("Server Bind Port : %d Failed!\n", HELLO_WORLD_SERVER_PORT);
+		exit(1);
+	}
+	printf("Server Bind Port : %d Success!\n", HELLO_WORLD_SERVER_PORT);
 
-  /* listen a socket */
-  if(listen(server_socket, LENGTH_OF_LISTEN_QUEUE))
-  {
-    printf("Server Listen Failed!\n");
-    exit(1);
-  }
-  printf("Server Listen Success!\n");
+	  /* listen a socket */
+	if (listen(server_socket, LENGTH_OF_LISTEN_QUEUE))
+	{
+		printf("Server Listen Failed!\n");
+		exit(1);
+	}
+	printf("Server Listen Success!\n");
 
-  pthread_t thread[NUM_THREADS];
-  int i = 0;
+	pthread_t thread[NUM_THREADS];
+	int i = 0;
 
-  /* run server */
-  while(1)
-  {
-    struct sockaddr_in client_addr;
-    int client_socket;
-    int nRet = 0;
-    socklen_t length;
+	  /* run server */
+	while (1)
+	{
+		struct sockaddr_in client_addr;
+		int client_socket;
+		int nRet = 0;
+		socklen_t length;
 
-    /* accept socket from client */
-    if(i < NUM_THREADS)
-    {
-      length = sizeof(client_addr);
-      client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &length);
-      if(client_socket < 0)
-      {
-        printf("Server Accept Failed!\n");
-        break;
-      }
-      printf("Server Accept Success!\n");
-	  printf("port : %u\n", ntohs(client_addr.sin_port));
-	  printf("client socket : [%d]\n", client_socket);
+		    /* accept socket from client */
+		if (i < NUM_THREADS)
+		{
+			length = sizeof(client_addr);
+			client_socket = accept(server_socket, (struct sockaddr*)&client_addr, &length);
+			if (client_socket < 0)
+			{
+				printf("Server Accept Failed!\n");
+				break;
+			}
+			printf("Server Accept Success!\n");
+			printf("port : %u\n", ntohs(client_addr.sin_port));
+			printf("client socket : [%d]\n", client_socket);
 
-      pthread_create(&thread[i], NULL, workthread, (void *)&client_socket);
-      ++i;
-    }
-    else
-    {
-      for (int j = 0; j < NUM_THREADS; j++)
-      {
-        pthread_join(thread[j], NULL);
-      }
-      i = 0;
-    }
-  }
-
-  close(server_socket);
-  return 0;
+			pthread_create(&thread[i], NULL, workthread, (void *)&client_socket);
+			++i;
+		}
+		else
+		{
+			for (int j = 0; j < NUM_THREADS; j++)
+			{
+				pthread_join(thread[j], NULL);
+			}
+			i = 0;
+		}
+	}
+	
+	close(server_socket);
+	return 0;
 }
 
 void *workthread(void *socket)
@@ -89,68 +90,71 @@ void *workthread(void *socket)
 	time_t CurTimer, LastTimer;
 	time(&LastTimer);
 
-    while(1)
-   {
-	  time(&CurTimer);
-	  if(CurTimer - LastTimer > WU_KEEP_ALIVE_TIMEOUT)
-	  {
-		  printf("keep alive time out\n");
-		  break;
-	  }
+	while (1)
+	{
+		time(&CurTimer);
+		if (CurTimer - LastTimer > WU_KEEP_ALIVE_TIMEOUT)
+		{
+			printf("keep alive time out\n");
+			break;
+		}
 
-	  Header hd;
-	  bzero(&hd, sizeof(Header));
-	  length = Net_Receive(client_socket, (char*)&hd, sizeof(Header), 0);
-	  if(length < 0) break;
-	  //else if(length == 0) continue;
+		Header hd;
+		bzero(&hd, sizeof(Header));
+		length = Net_Receive(client_socket, (char*)&hd, sizeof(Header), 0);
+		if (length < 0) break;
+		//else if(length == 0) continue;
 
-	  if(memcmp(hd.szFlag, WU_HEADER_FLAG, WU_HEADER_FLAG_LEN) != 0)
-	  {
-		  printf("invalid header flag; [%0x],[%0x],[%0x],[%0x]\n", hd.szFlag[0], hd.szFlag[1],
-			  hd.szFlag[2], hd.szFlag[3]);
+		if (memcmp(hd.szFlag, WU_HEADER_FLAG, WU_HEADER_FLAG_LEN) != 0)
+		{
+			printf("invalid header flag; [%0x],[%0x],[%0x],[%0x]\n",
+				hd.szFlag[0],
+				hd.szFlag[1],
+				hd.szFlag[2],
+				hd.szFlag[3]);
 		  
-		  bool bFlag = false;
-		  if(SearchPacketHeader(client_socket, (char *)hd.szFlag, WU_HEADER_FLAG_LEN))
-		  {
-			  length = Net_Receive(client_socket, (char*)&hd.usCode, sizeof(Header) - WU_HEADER_FLAG_LEN, 0); 
-			  if(length == sizeof(Header) - WU_HEADER_FLAG_LEN)
-				  bFlag = true;
-		  }
-		  if(!bFlag) break;
-	  }
+			bool bFlag = false;
+			if (SearchPacketHeader(client_socket, (char *)hd.szFlag, WU_HEADER_FLAG_LEN))
+			{
+				length = Net_Receive(client_socket, (char*)&hd.usCode, sizeof(Header) - WU_HEADER_FLAG_LEN, 0); 
+				if (length == sizeof(Header) - WU_HEADER_FLAG_LEN)
+					bFlag = true;
+			}
+			if (!bFlag) break;
+		}
 
-	  unsigned int uiDataLen = hd.uiTotalLength - WU_HEADER_LEN;
-	  char *pDataBuff = (char *)malloc(uiDataLen);
-	  if(pDataBuff == NULL)
-	  {
-		  printf("Insufficient memory available\n");
-		  break;
-	  }
-	  bzero(pDataBuff, uiDataLen);
+		unsigned int uiDataLen = hd.uiTotalLength - WU_HEADER_LEN;
+		char *pDataBuff = (char *)malloc(uiDataLen);
+		if (pDataBuff == NULL)
+		{
+			printf("Insufficient memory available\n");
+			break;
+		}
+		bzero(pDataBuff, uiDataLen);
 
-	  length = Net_Receive(client_socket, pDataBuff, uiDataLen, 0);
-	  if(length < 0) break;
-	  //else if(length == 0) continue;
+		length = Net_Receive(client_socket, pDataBuff, uiDataLen, 0);
+		if (length < 0) break;
+		//else if(length == 0) continue;
 
-	  switch(hd.usCode)
-	  {
-	  case KEEP_ALIVE_REQ:
-		  {
-			  time(&LastTimer);
-			  Keep_Alive_Req_Function(client_socket, &hd, pDataBuff);
-		  }
-		  break;
-	  case LOGIN_REQ:
-		  {
-			  Login_Req_Function(client_socket, &hd, pDataBuff);
-		  }
-		  break;
-	  default:
-		  break;
-	  }
+		switch (hd.usCode)
+		{
+		case KEEP_ALIVE_REQ:
+			{
+				time(&LastTimer);
+				Keep_Alive_Req_Function(client_socket, &hd, pDataBuff);
+			}
+			break;
+		case LOGIN_REQ:
+			{
+				Login_Req_Function(client_socket, &hd, pDataBuff);
+			}
+			break;
+		default:
+			break;
+		}
 
-	  free(pDataBuff);
-   }
+		free(pDataBuff);
+	}
 
-   close(client_socket);
+	close(client_socket);
 }
